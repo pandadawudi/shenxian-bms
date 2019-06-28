9fd38f4a2f999e801caf9b7db49270fab551126f
panjin <qiudaoyu1100>
2019-06-28 15:38:16 +0800

    增加User控制器

---
+++ src/main/java/cn/panjin/shenxianbms/base/user/controller/BaseUserController.java
@@ -0,0 +1,21 @@
+package cn.panjin.shenxianbms.base.user.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ * User控制器
+ * </p>
+ *
+ * @Anthor panjin
+ * @Description
+ * @Date 2019/6/28 0028 15:36
+ * @Version 1.0
+ */
+@RestController
+@Slf4j
+public class BaseUserController {
+
+
+}
